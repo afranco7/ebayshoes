32c928bc07fcb5d6f92c2e69e683eb3df8217aef
afranco7 <afranco7@gmail.com>
2018-02-24 00:42:08 -0500

    migrate to typescript

--- protractor.config.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-    framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['test/Ebay.spec.js'],
-    onPrepare: () => {
-      browser.ignoreSynchronization = true;
-    }
-   }
