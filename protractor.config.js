54a65558076596c931d894d97af360b43974cb29
afranco7 <afranco7@gmail.com>
2018-02-24 00:16:43 -0500

    first test Ebay

--- protractor.config.js
+++ protractor.config.js
@@ -1,5 +1,8 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['test/spec.js']
+    specs: ['test/Ebay.spec.js'],
+    onPrepare: () => {
+      browser.ignoreSynchronization = true;
+    }
    }
---
+++ test/Ebay.spec.js
@@ -0,0 +1,11 @@
+describe('Given a SDET learning protractor', () => {
+  describe('when open Ebay Page', () => {
+    beforeEach(() => {
+      browser.get('http://www.ebay.com');
+    });
+     
+    it('then should have a title', () => {
+      expect(browser.getTitle()).toEqual('Electronics, Cars, Fashion, Collectibles, Coupons and More | eBay');
+    });
+  });
+ });
