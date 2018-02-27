9f8b229e9930f2fcf5b08157fbd29b85ee0881c5
Alejandro Franco Barrios <AFrancoB@pslcol.com.co>
2018-02-27 13:38:23 -0500

    tslint changes

--- protractor/config.ts
+++ protractor/config.ts
@@ -2,24 +2,24 @@ import { browser, Config } from 'protractor';
 import { reporter }   from './helpers/reporter';
 
 export const config: Config = {
- framework: 'jasmine',
- SELENIUM_PROMISE_MANAGER: false,
- specs: ['../test/**/*.spec.js'],
- getPageTimeout: 50000,
- noGlobals: true,
- capabilities: {
-  browserName: 'chrome',
-  chromeOptions: {
-    args: ['disable-infobars=true --window-size=800,600'],
-    prefs: { credentials_enable_service: false }
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 50000,
+  noGlobals: true,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['disable-infobars=true --window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(30000);
+    reporter();
   }
-},
-jasmineNodeOpts: {
-  defaultTimeoutInterval: 120000
-},
- onPrepare: () => {
-  browser.ignoreSynchronization = true;
-  browser.manage().timeouts().implicitlyWait(30000);
-  reporter();
- }
-}
+};
--- protractor/headless.conf.ts
+++ protractor/headless.conf.ts
@@ -2,20 +2,20 @@ import { browser, Config } from 'protractor';
 import { reporter }   from './helpers/reporter';
 
 export const config: Config = {
- framework: 'jasmine',
- SELENIUM_PROMISE_MANAGER: false,
- specs: ['../test/**/*.spec.js'],
- getPageTimeout: 30000,
- noGlobals: true,
- ignoreUncaughtExceptions: true,
- capabilities: {
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 30000,
+  noGlobals: true,
+  ignoreUncaughtExceptions: true,
+  capabilities: {
     browserName: 'chrome',
     chromeOptions: {
       args: ['--headless', '--disable-gpu', '--window-size=800,600','--no-sandbox']
     }
   }, 
- onPrepare: () => {
-   browser.ignoreSynchronization = true;
-   reporter();
- } 
-}
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  } 
+};
